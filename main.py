2445542180e5a33dd19ad2757a4cc15f3464409d
Arturo Suárez <manuel.suarez@cimat.mx>
2022-11-22 10:43:01 -0600

    Import

---
+++ main.py
@@ -0,0 +1,9 @@
+# Import
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+import cv2
+#from PIL import Image
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
