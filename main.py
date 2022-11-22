a2d3a9970a0f803a28148bdd1d09080d61c93913
Arturo Suárez <manuel.suarez@cimat.mx>
2022-11-22 10:49:14 -0600

    Definimos las aumentaciones a aplicar y visualizamos

--- main.py
+++ main.py
@@ -173,3 +173,93 @@ visualize(
     oil_spill=mask[..., 0].squeeze(),
     background_mask=mask[..., 1].squeeze(),
 )
+
+# Data augmentation
+import albumentations as A
+
+
+def round_clip_0_1(x, **kwargs):
+    return x.round().clip(0, 1)
+
+
+# define heavy augmentations
+def get_training_augmentation():
+    train_transform = [
+
+        A.HorizontalFlip(p=0.5),
+
+        A.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=1, border_mode=0),
+
+        A.PadIfNeeded(min_height=320, min_width=320, always_apply=True, border_mode=0),
+        A.RandomCrop(height=320, width=320, always_apply=True),
+
+        A.IAAAdditiveGaussianNoise(p=0.2),
+        A.IAAPerspective(p=0.5),
+
+        A.OneOf(
+            [
+                A.CLAHE(p=1),
+                A.RandomBrightness(p=1),
+                A.RandomGamma(p=1),
+            ],
+            p=0.9,
+        ),
+
+        A.OneOf(
+            [
+                A.IAASharpen(p=1),
+                A.Blur(blur_limit=3, p=1),
+                A.MotionBlur(blur_limit=3, p=1),
+            ],
+            p=0.9,
+        ),
+
+        A.OneOf(
+            [
+                A.RandomContrast(p=1),
+                # A.HueSaturationValue(p=1),
+            ],
+            p=0.9,
+        ),
+        A.Lambda(mask=round_clip_0_1)
+    ]
+    return A.Compose(train_transform)
+
+
+def get_validation_augmentation():
+    """Add paddings to make image shape divisible by 32"""
+    test_transform = [
+        A.PadIfNeeded(384, 480)
+    ]
+    return A.Compose(test_transform)
+
+
+def get_preprocessing(preprocessing_fn):
+    """Construct preprocessing transform
+
+    Args:
+        preprocessing_fn (callbale): data normalization function
+            (can be specific for each pretrained neural network)
+    Return:
+        transform: albumentations.Compose
+
+    """
+
+    _transform = [
+        A.Lambda(image=preprocessing_fn),
+    ]
+    return A.Compose(_transform)
+
+# Aplicamos y visualizamos aumentación de datos
+# Lets look at augmented data we have
+#dataset = Dataset(x_train_dir, y_train_dir, classes=['sea_surface', 'oil_spill', 'look_alike', 'ship', 'land'], augmentation=get_training_augmentation())
+dataset = Dataset(x_train_dir, y_train_dir, classes=['sea_surface', 'oil_spill'], augmentation=get_training_augmentation())
+
+image, mask = dataset[12] # get some sample
+print(image.shape, mask.shape)
+visualize(
+    image=image,
+    sea_surface=mask[..., 0].squeeze(),
+    oil_spill=mask[..., 1].squeeze(),
+    background_mask=mask[..., 2].squeeze(),
+)
