a7d32925537e3720b74e6aed47d513eaa9cb02f3
Arturo Suárez <manuel.suarez@cimat.mx>
2022-11-22 10:46:48 -0600

    Configuración de los conjuntos de entrenamiento, validación y pruebas

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ import matplotlib.pyplot as plt
 # Dataset directory
 DATA_DIR = '/home/est_posgrado_manuel.suarez/data/oil-spill-dataset'
 
+# Dataset's configuration
+x_train_dir = os.path.join(DATA_DIR, 'train', 'images')
+y_train_dir = os.path.join(DATA_DIR, 'train', 'labels_1D')
+
+x_valid_dir = os.path.join(DATA_DIR, 'val', 'images')
+y_valid_dir = os.path.join(DATA_DIR, 'val', 'labels_1D')
+
+x_test_dir = os.path.join(DATA_DIR, 'test', 'images')
+y_test_dir = os.path.join(DATA_DIR, 'test', 'labels_1D')
