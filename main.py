f44bb669efb14ffbd2eb2f8bd08712bc5fcdb6e9
Arturo Suárez <manuel.suarez@cimat.mx>
2022-11-23 16:29:50 -0600

    Cambiamos la operación de agregación (puede ser el orígen del error por la concatenación)

--- main.py
+++ main.py
@@ -306,7 +306,7 @@ for BACKBONE in BACKBONES:
     activation = 'sigmoid' if n_classes == 1 else 'softmax'
 
     #create model
-    model = sm.PSPNet(BACKBONE, classes=n_classes, activation=activation)
+    model = sm.PSPNet(BACKBONE, classes=n_classes, activation=activation, pyramid_aggregation="sum")
 
     # define optomizer
     optim = keras.optimizers.Adam(LR)
