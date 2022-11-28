cacb050778ee0c3cf580d75252e6d556c54a0bf8
Arturo Suárez <manuel.suarez@cimat.mx>
2022-11-28 10:16:27 -0600

    Probamos FPN con mas arquitecturas y épocas

--- main.py
+++ main.py
@@ -306,7 +306,7 @@ for BACKBONE in BACKBONES:
     activation = 'sigmoid' if n_classes == 1 else 'softmax'
 
     #create model
-    model = sm.Linknet(BACKBONE, classes=n_classes, activation=activation, )
+    model = sm.FPN(BACKBONE, classes=n_classes, activation=activation, )
 
     # define optomizer
     optim = keras.optimizers.Adam(LR)
